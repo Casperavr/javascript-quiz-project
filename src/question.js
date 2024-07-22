f7782d3e0a722055b74b6f717cf5e0a69bbde595
Casperavr <casper@hhc.earth>
2024-07-22 17:30:30 +0200

    implemented the question class and quiz class

--- src/question.js
+++ src/question.js
@@ -4,4 +4,21 @@ class Question {
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
-}
+
+
+    constructor(text, choices, answer, difficulty){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+    shuffleChoices(){
+        let tempChoices = this.choices.slice();
+        this.choices = [];
+        while(tempChoices.length > 0){
+            let randomIndex = Math.floor(Math.random() * tempChoices.length);
+            this.choices.push(tempChoices.splice(randomIndex, 1)[0]);
+        };
+    }
+};
--- src/quiz.js
+++ src/quiz.js
@@ -12,4 +12,60 @@ class Quiz {
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
+
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
+
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
+
+    shuffleQuestions(){
+        let tempQuestions = this.questions.slice();
+        this.questions = [];
+        while(tempQuestions.length > 0){
+            let randomIndex = Math.floor(Math.random() * tempQuestions.length);
+            this.questions.push(tempQuestions.splice(randomIndex, 1)[0]);
+        };
+    }
+
+    checkAnswer(answer){
+        if(answer === this.questions[this.currentQuestionIndex].answer){
+            this.correctAnswers++;
+        }
+    }
+
+    hasEnded(){
+        return this.currentQuestionIndex < this.questions.length ? false : true
+    }
+
+    filterQuestionsByDifficulty(difficulty){
+        if(difficulty > 0 && difficulty < 4){
+            let tempQuestionsByDifficulty = [];
+            for(let i = 0; i < this.questions.length; i++){
+                if(this.questions[i].difficulty === difficulty){
+                    tempQuestionsByDifficulty.push(this.questions[i])
+                }
+            }   
+            this.questions = tempQuestionsByDifficulty;
+        }
+    }
+
+    averageDifficulty(){
+        let sum = 0
+        this.questions.forEach(function(question){
+            sum += question.difficulty;
+        });
+        return sum / this.questions.length;
+    }
+    
 }
